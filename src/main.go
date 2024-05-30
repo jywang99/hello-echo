3886c68cc2dadd9d5324d5b00661fb083c81c57a
Junyi Wang <junyi.wang.007@gmail.com>
2024-05-29 22:25:20 -0600

    tutorial done

--- src/main.go
+++ src/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"html/template"
 	"io"
+	"strconv"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -12,7 +14,7 @@ type Templates struct {
     Templates *template.Template
 }
 
-func (t *Templates) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+func (t *Templates) Render(w io.Writer, name string, data any, c echo.Context) error {
     return t.Templates.ExecuteTemplate(w, name, data)
 }
 
@@ -22,13 +24,17 @@ func newTemplate() *Templates {
     }
 }
 
+var id = 0
 type Contact struct {
+    Id int
     Name string
     Email string
 }
 
 func newContact(name, email string) Contact {
+    id ++
     return Contact{
+        Id: id,
         Name: name,
         Email: email,
     }
@@ -38,6 +44,15 @@ type Contacts = []Contact
 
 type Data struct {
     Contacts Contacts
+}
+
+func (d *Data) indexOf(id int) int {
+    for i, contact := range d.Contacts {
+        if contact.Id == id {
+            return i
+        }
+    }
+    return -1
 }
 
 func newData() Data {
@@ -107,9 +122,28 @@ func main() {
             return c.Render(422, "form", data)
         }
 
-        page.Data.Contacts = append(page.Data.Contacts, newContact(name, email))
+        contact := newContact(name, email)
+        page.Data.Contacts = append(page.Data.Contacts, contact)
 
-        return c.Render(200, "display", page.Data)
+        c.Render(200, "form", newFormData())
+        return c.Render(200, "oob-contact", contact)
+    })
+
+    e.DELETE("/contacts/:id", func(c echo.Context) error {
+        time.Sleep(3 * time.Second)
+        idStr := c.Param("id")
+        id, err := strconv.Atoi(idStr)
+        if err != nil {
+            return c.String(400, "Invalid ID")
+        }
+
+        idx := page.Data.indexOf(id)
+        if idx == -1 {
+            return c.String(404, "Contact not found")
+        }
+
+        page.Data.Contacts = append(page.Data.Contacts[:idx], page.Data.Contacts[idx+1:]...)
+        return c.NoContent(200)
     })
 
     e.Logger.Fatal(e.Start(":42069"))
